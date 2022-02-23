62873b3a0f51b8c872709aa071d279120419589d
Michael Chung <mikechung89@gmail.com>
2022-02-23 12:37:06 -0500

    commit tree 24494972752affc11747b5e64496eaee3c3e366c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let thanks = [];
+    for(let name of names) {
+        thanks.push(`Thank you, ${name}, for the wonderful surprise gift!`)
+    }
+    return thanks
+}
+
+function countDown(n) {
+    while(n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
